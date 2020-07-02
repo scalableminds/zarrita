f66871a3573cb817f8f514a7b77cbd139d6733ac
Alistair Miles <alimanfoo@googlemail.com>
2020-07-02 18:18:51 +0100

    Iteration 2 (#2)
    * add name property
    
    * reduce example array size
    
    * setup array tests
    
    * array basic selections
    
    * tweak test
    
    * test with compression
    
    * tree no report

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-**Here be dragons.**
+**Here be dragons.** Minimal exploratory implementation of the Zarr version 3.0 core protocol. 
+This is a technical spike, not for production use.
 
 Ensure blank slate:
 
@@ -16,13 +17,10 @@ Create a new hierarchy stored on the local file system:
 >>> h
 <zarr_v3 Hierarchy>
 >>> from sh import tree, cat
->>> tree('test.zr3', '-n')
+>>> tree('test.zr3', '-n', '--noreport')
 test.zr3
 └── zarr.json
 <BLANKLINE>
-0 directories, 1 file
-<BLANKLINE>
-
 >>> cat('test.zr3/zarr.json')
 {
     "zarr_format": "https://purl.org/zarr/spec/protocol/core/3.0",
@@ -44,24 +42,29 @@ Access a previously created hierarchy:
 Create an array:
 
 ```
->>> a = h.create_array('/arthur/dent', shape=(100, 10), dtype='i4', chunk_shape=(20, 5), compressor=None, attrs={'question': 'life', 'answer': 42})
+>>> from numcodecs import GZip
+>>> compressor = GZip(level=1)
+>>> attrs = {'question': 'life', 'answer': 42}
+>>> a = h.create_array('/arthur/dent', shape=(5, 10), dtype='i4', chunk_shape=(2, 5), compressor=compressor, attrs=attrs)
 >>> a
 <zarr_v3 Array /arthur/dent>
 >>> a.path
 '/arthur/dent'
+>>> a.name
+'dent'
 >>> a.ndim
 2
 >>> a.shape
-(100, 10)
+(5, 10)
 >>> a.dtype
 dtype('int32')
 >>> a.chunk_shape
-(20, 5)
->>> a.compressor is None
-True
+(2, 5)
+>>> a.compressor
+GZip(level=1)
 >>> a.attrs
 {'question': 'life', 'answer': 42}
->>> tree('test.zr3', '-n')
+>>> tree('test.zr3', '-n', '--noreport')
 test.zr3
 ├── meta
 │   └── root
@@ -69,25 +72,27 @@ test.zr3
 │           └── dent.array
 └── zarr.json
 <BLANKLINE>
-3 directories, 2 files
-<BLANKLINE>
-
 >>> cat('test.zr3/meta/root/arthur/dent.array')
 {
     "shape": [
-        100,
+        5,
         10
     ],
     "data_type": "<i4",
     "chunk_grid": {
         "type": "regular",
         "chunk_shape": [
-            20,
+            2,
             5
         ]
     },
     "chunk_memory_layout": "C",
-    "compressor": null,
+    "compressor": {
+        "codec": "https://purl.org/zarr/spec/codec/gzip/1.0",
+        "configuration": {
+            "level": 1
+        }
+    },
     "fill_value": null,
     "extensions": [],
     "attributes": {
@@ -101,12 +106,17 @@ test.zr3
 Create a group:
 
 ```
->>> g = h.create_group('/tricia/mcmillan', attrs={'heart': 'gold', 'improbability': 'infinite'})
+>>> attrs = {'heart': 'gold', 'improbability': 'infinite'}
+>>> g = h.create_group('/tricia/mcmillan', attrs=attrs)
 >>> g
 <zarr_v3 Group /tricia/mcmillan>
+>>> g.path
+'/tricia/mcmillan'
+>>> g.name
+'mcmillan'
 >>> g.attrs
 {'heart': 'gold', 'improbability': 'infinite'}
->>> tree('test.zr3', '-n')
+>>> tree('test.zr3', '-n', '--noreport')
 test.zr3
 ├── meta
 │   └── root
@@ -116,9 +126,6 @@ test.zr3
 │           └── mcmillan.group
 └── zarr.json
 <BLANKLINE>
-4 directories, 3 files
-<BLANKLINE>
-
 >>> cat('test.zr3/meta/root/tricia/mcmillan.group')
 {
     "extensions": [],
@@ -137,13 +144,13 @@ Access an array:
 >>> a
 <zarr_v3 Array /arthur/dent>
 >>> a.shape
-(100, 10)
+(5, 10)
 >>> a.dtype
 dtype('int32')
 >>> a.chunk_shape
-(20, 5)
->>> a.compressor is None
-True
+(2, 5)
+>>> a.compressor
+GZip(level=1)
 >>> a.attrs
 {'question': 'life', 'answer': 42}
 
@@ -219,5 +226,162 @@ Alternative way to explore the hierarchy:
 []
 >>> root['arthur'].list_children()
 [{'name': 'dent', 'type': 'array'}]
+
+```
+
+Read and write data into an array:
+
+```
+>>> import numpy as np
+>>> a = h['/arthur/dent']
+>>> a
+<zarr_v3 Array /arthur/dent>
+>>> tree('test.zr3', '-n', '--noreport')
+test.zr3
+├── meta
+│   └── root
+│       ├── arthur
+│       │   └── dent.array
+│       └── tricia
+│           └── mcmillan.group
+└── zarr.json
+<BLANKLINE>
+>>> a[:, :]
+array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]], dtype=int32)
+>>> a[...]
+array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]], dtype=int32)
+>>> a[:]
+array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]], dtype=int32)
+>>> a[0, :] = 42
+>>> a[:]
+array([[42, 42, 42, 42, 42, 42, 42, 42, 42, 42],
+       [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+       [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+       [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+       [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0]], dtype=int32)
+>>> tree('test.zr3', '-n', '--noreport')
+test.zr3
+├── data
+│   └── arthur
+│       └── dent
+│           ├── 0.0
+│           └── 0.1
+├── meta
+│   └── root
+│       ├── arthur
+│       │   └── dent.array
+│       └── tricia
+│           └── mcmillan.group
+└── zarr.json
+<BLANKLINE>
+>>> a[:, 0] = 42
+>>> a[:]
+array([[42, 42, 42, 42, 42, 42, 42, 42, 42, 42],
+       [42,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+       [42,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+       [42,  0,  0,  0,  0,  0,  0,  0,  0,  0],
+       [42,  0,  0,  0,  0,  0,  0,  0,  0,  0]], dtype=int32)
+>>> tree('test.zr3', '-n', '--noreport')
+test.zr3
+├── data
+│   └── arthur
+│       └── dent
+│           ├── 0.0
+│           ├── 0.1
+│           ├── 1.0
+│           └── 2.0
+├── meta
+│   └── root
+│       ├── arthur
+│       │   └── dent.array
+│       └── tricia
+│           └── mcmillan.group
+└── zarr.json
+<BLANKLINE>
+>>> a[:] = 42
+>>> a[:]
+array([[42, 42, 42, 42, 42, 42, 42, 42, 42, 42],
+       [42, 42, 42, 42, 42, 42, 42, 42, 42, 42],
+       [42, 42, 42, 42, 42, 42, 42, 42, 42, 42],
+       [42, 42, 42, 42, 42, 42, 42, 42, 42, 42],
+       [42, 42, 42, 42, 42, 42, 42, 42, 42, 42]], dtype=int32)
+>>> tree('test.zr3', '-n', '--noreport')
+test.zr3
+├── data
+│   └── arthur
+│       └── dent
+│           ├── 0.0
+│           ├── 0.1
+│           ├── 1.0
+│           ├── 1.1
+│           ├── 2.0
+│           └── 2.1
+├── meta
+│   └── root
+│       ├── arthur
+│       │   └── dent.array
+│       └── tricia
+│           └── mcmillan.group
+└── zarr.json
+<BLANKLINE>
+>>> a[0, :] = np.arange(10)
+>>> a[:]
+array([[ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9],
+       [42, 42, 42, 42, 42, 42, 42, 42, 42, 42],
+       [42, 42, 42, 42, 42, 42, 42, 42, 42, 42],
+       [42, 42, 42, 42, 42, 42, 42, 42, 42, 42],
+       [42, 42, 42, 42, 42, 42, 42, 42, 42, 42]], dtype=int32)
+>>> a[:, 0] = np.arange(0, 50, 10)
+>>> a[:]
+array([[ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9],
+       [10, 42, 42, 42, 42, 42, 42, 42, 42, 42],
+       [20, 42, 42, 42, 42, 42, 42, 42, 42, 42],
+       [30, 42, 42, 42, 42, 42, 42, 42, 42, 42],
+       [40, 42, 42, 42, 42, 42, 42, 42, 42, 42]], dtype=int32)
+>>> a[:] = np.arange(50).reshape(5, 10)
+>>> a[:]
+array([[ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9],
+       [10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
+       [20, 21, 22, 23, 24, 25, 26, 27, 28, 29],
+       [30, 31, 32, 33, 34, 35, 36, 37, 38, 39],
+       [40, 41, 42, 43, 44, 45, 46, 47, 48, 49]], dtype=int32)
+>>> a[:, 0]
+array([ 0, 10, 20, 30, 40], dtype=int32)
+>>> a[:, 1]
+array([ 1, 11, 21, 31, 41], dtype=int32)
+>>> a[0, :]
+array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], dtype=int32)
+>>> a[1, :]
+array([10, 11, 12, 13, 14, 15, 16, 17, 18, 19], dtype=int32)
+>>> a[:, 0:7]
+array([[ 0,  1,  2,  3,  4,  5,  6],
+       [10, 11, 12, 13, 14, 15, 16],
+       [20, 21, 22, 23, 24, 25, 26],
+       [30, 31, 32, 33, 34, 35, 36],
+       [40, 41, 42, 43, 44, 45, 46]], dtype=int32)
+>>> a[0:3, :]
+array([[ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9],
+       [10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
+       [20, 21, 22, 23, 24, 25, 26, 27, 28, 29]], dtype=int32)
+>>> a[0:3, 0:7]
+array([[ 0,  1,  2,  3,  4,  5,  6],
+       [10, 11, 12, 13, 14, 15, 16],
+       [20, 21, 22, 23, 24, 25, 26]], dtype=int32)
+>>> a[1:4, 2:7]
+array([[12, 13, 14, 15, 16],
+       [22, 23, 24, 25, 26],
+       [32, 33, 34, 35, 36]], dtype=int32)
 
 ```
--- zarr_v3.py
+++ zarr_v3.py
@@ -1,8 +1,17 @@
+# standard library dependencies
 import json
+import numbers
+import itertools
+import collections
+import math
+
+
+# third-party dependencies
 import fsspec
 import numpy as np
 import numcodecs
 from numcodecs.abc import Codec
+from numcodecs.compat import ensure_ndarray
 
 
 def _json_encode(o):
@@ -134,7 +143,7 @@ def _encode_codec(codec):
     # only support gzip for now
     assert codec.codec_id in {'gzip'}
     config = codec.get_config()
-    del config['codec_id']
+    del config['id']
     meta = {
         'codec': 'https://purl.org/zarr/spec/codec/gzip/1.0',
         'configuration': config,
@@ -149,7 +158,7 @@ def _decode_codec(meta):
     # only support gzip for now
     if meta['codec'] != 'https://purl.org/zarr/spec/codec/gzip/1.0':
         raise NotImplementedError
-    codec = numcodecs.GZip(level=meta['level'])
+    codec = numcodecs.GZip(level=meta['configuration']['level'])
     return codec
 
 
@@ -385,6 +394,10 @@ class Group:
         self.path = path
         self.owner = owner
 
+    @property
+    def name(self):
+        return self.path.split('/')[-1]
+
     def list_children(self):
         return self.owner.list_children(self.path)
 
@@ -472,17 +485,461 @@ class Array:
     def ndim(self):
         return len(self.shape)
 
-    def __getitem__(self, item):
-        # TODO
-        pass
+    @property
+    def name(self):
+        return self.path.split('/')[-1]
 
-    def __setitem__(self, item, value):
-        # TODO
-        pass
+    def __getitem__(self, selection):
+        return self.get_basic_selection(selection)
+
+    def get_basic_selection(self, selection):
+        indexer = BasicIndexer(selection, shape=self.shape, chunk_shape=self.chunk_shape)
+        return self._get_selection(indexer=indexer)
+
+    def _get_selection(self, indexer):
+
+        # setup output array
+        out = np.zeros(indexer.shape, dtype=self.dtype, order='C')
+
+        # iterate over chunks
+        for chunk_coords, chunk_selection, out_selection in indexer:
+
+            # load chunk selection into output array
+            self._chunk_getitem(chunk_coords, chunk_selection, out, out_selection)
+
+        if out.shape:
+            return out
+        else:
+            return out[()]
+
+    def _chunk_getitem(self, chunk_coords, chunk_selection, out, out_selection):
+
+        # obtain key for chunk
+        chunk_key = self._chunk_key(chunk_coords)
+
+        try:
+            # obtain encoded data for chunk
+            encoded_chunk_data = self.store[chunk_key]
+
+        except KeyError:
+            # chunk not initialized, maybe fill
+            if self.fill_value is not None:
+                out[out_selection] = self.fill_value
+
+        else:
+            # decode chunk
+            chunk = self._decode_chunk(encoded_chunk_data)
+
+            # select data from chunk
+            tmp = chunk[chunk_selection]
+
+            # store selected data in output
+            out[out_selection] = tmp
+
+    def _chunk_key(self, chunk_coords):
+        suffix = '.'.join(map(str, chunk_coords))
+        if self.path == '/':
+            # special case array as root node
+            key = f'data/{suffix}'
+        else:
+            key = f'data{self.path}/{suffix}'
+        return key
+
+    def _decode_chunk(self, encoded_data):
+
+        # decompress
+        if self.compressor is not None:
+            chunk = self.compressor.decode(encoded_data)
+        else:
+            chunk = encoded_data
+
+        # view as numpy array with correct dtype
+        chunk = ensure_ndarray(chunk)
+        chunk = chunk.view(self.dtype)
+
+        # ensure correct chunk shape
+        chunk = chunk.reshape(-1, order='A')
+        chunk = chunk.reshape(self.chunk_shape, order='C')
+
+        return chunk
+
+    def __setitem__(self, selection, value):
+        self.set_basic_selection(selection, value)
+
+    def set_basic_selection(self, selection, value):
+        indexer = BasicIndexer(selection, shape=self.shape, chunk_shape=self.chunk_shape)
+        self._set_selection(indexer, value)
+
+    def _set_selection(self, indexer, value):
+
+        # We iterate over all chunks which overlap the selection and thus contain data
+        # that needs to be replaced. Each chunk is processed in turn, extracting the
+        # necessary data from the value array and storing into the chunk array.
+
+        # N.B., it is an important optimisation that we only visit chunks which overlap
+        # the selection. This minimises the number of iterations in the main for loop.
+
+        # determine indices of chunks overlapping the selection
+        sel_shape = indexer.shape
+
+        # check value shape
+        if sel_shape == ():
+            # setting a single item
+            assert np.isscalar(value)
+        elif np.isscalar(value):
+            # setting a scalar value
+            pass
+        else:
+            if not hasattr(value, 'shape'):
+                value = np.asarray(value, self.dtype)
+            assert value.shape == sel_shape
+
+        # iterate over chunks in range
+        for chunk_coords, chunk_selection, out_selection in indexer:
+
+            # extract data to store
+            if sel_shape == ():
+                chunk_value = value
+            elif np.isscalar(value):
+                chunk_value = value
+            else:
+                chunk_value = value[out_selection]
+
+            # put data
+            self._chunk_setitem(chunk_coords, chunk_selection, chunk_value)
+
+    def _chunk_setitem(self, chunk_coords, chunk_selection, value):
+
+        # obtain key for chunk storage
+        chunk_key = self._chunk_key(chunk_coords)
+
+        if is_total_slice(chunk_selection, self.chunk_shape):
+            # totally replace chunk
+
+            # optimization: we are completely replacing the chunk, so no need
+            # to access the existing chunk data
+
+            if np.isscalar(value):
+
+                # setup array filled with value
+                chunk = np.empty(self.chunk_shape, dtype=self.dtype, order='C')
+                chunk.fill(value)
+
+            else:
+
+                # ensure array is contiguous
+                chunk = value.astype(self.dtype, order='C', copy=False)
+
+        else:
+            # partially replace the contents of this chunk
+
+            try:
+
+                # obtain compressed data for chunk
+                encoded_chunk_data = self.store[chunk_key]
+
+            except KeyError:
+
+                # chunk not initialized
+                if self.fill_value is not None:
+                    chunk = np.empty(self.chunk_shape, dtype=self.dtype, order='C')
+                    chunk.fill(self.fill_value)
+                else:
+                    # N.B., use zeros here so any region beyond the array has consistent
+                    # and compressible data
+                    chunk = np.zeros(self.chunk_shape, dtype=self.dtype, order='C')
+
+            else:
+
+                # decode chunk
+                chunk = self._decode_chunk(encoded_chunk_data)
+                if not chunk.flags.writeable:
+                    chunk = chunk.copy(order='K')
+
+            # modify
+            chunk[chunk_selection] = value
+
+        # encode chunk
+        encoded_chunk_data = self._encode_chunk(chunk)
+
+        # store
+        self.store[chunk_key] = encoded_chunk_data
+
+    def _encode_chunk(self, chunk):
+
+        # compress
+        if self.compressor is not None:
+            encoded_chunk_data = self.compressor.encode(chunk)
+        else:
+            encoded_chunk_data = chunk
+
+        return encoded_chunk_data
 
     def __repr__(self):
         path = self.path
         return f'<zarr_v3 Array {path}>'
+
+
+def is_total_slice(item, shape):
+    """Determine whether `item` specifies a complete slice of array with the
+    given `shape`. Used to optimize __setitem__ operations on the Chunk
+    class."""
+
+    # N.B., assume shape is normalized
+
+    if item == Ellipsis:
+        return True
+    if item == slice(None):
+        return True
+    if isinstance(item, slice):
+        item = item,
+    if isinstance(item, tuple):
+        return all(
+            (isinstance(s, slice) and
+                ((s == slice(None)) or
+                 ((s.stop - s.start == l) and (s.step in [1, None]))))
+            for s, l in zip(item, shape)
+        )
+    else:
+        raise TypeError('expected slice or tuple of slices, found %r' % item)
+
+
+def ensure_tuple(v):
+    if not isinstance(v, tuple):
+        v = (v,)
+    return v
+
+
+def err_too_many_indices(selection, shape):
+    raise IndexError('too many indices for array; expected {}, got {}'
+                     .format(len(shape), len(selection)))
+
+
+def err_boundscheck(dim_len):
+    raise IndexError('index out of bounds for dimension with length {}'
+                     .format(dim_len))
+
+
+def err_negative_step():
+    raise IndexError('only slices with step >= 1 are supported')
+
+
+def check_selection_length(selection, shape):
+    if len(selection) > len(shape):
+        err_too_many_indices(selection, shape)
+
+
+def replace_ellipsis(selection, shape):
+
+    selection = ensure_tuple(selection)
+
+    # count number of ellipsis present
+    n_ellipsis = sum(1 for i in selection if i is Ellipsis)
+
+    if n_ellipsis > 1:
+        # more than 1 is an error
+        raise IndexError("an index can only have a single ellipsis ('...')")
+
+    elif n_ellipsis == 1:
+        # locate the ellipsis, count how many items to left and right
+        n_items_l = selection.index(Ellipsis)  # items to left of ellipsis
+        n_items_r = len(selection) - (n_items_l + 1)  # items to right of ellipsis
+        n_items = len(selection) - 1  # all non-ellipsis items
+
+        if n_items >= len(shape):
+            # ellipsis does nothing, just remove it
+            selection = tuple(i for i in selection if i != Ellipsis)
+
+        else:
+            # replace ellipsis with as many slices are needed for number of dims
+            new_item = selection[:n_items_l] + ((slice(None),) * (len(shape) - n_items))
+            if n_items_r:
+                new_item += selection[-n_items_r:]
+            selection = new_item
+
+    # fill out selection if not completely specified
+    if len(selection) < len(shape):
+        selection += (slice(None),) * (len(shape) - len(selection))
+
+    # check selection not too long
+    check_selection_length(selection, shape)
+
+    return selection
+
+
+ChunkDimProjection = collections.namedtuple(
+    'ChunkDimProjection',
+    ('dim_chunk_ix', 'dim_chunk_sel', 'dim_out_sel')
+)
+"""A mapping from chunk to output array for a single dimension.
+Parameters
+----------
+dim_chunk_ix
+    Index of chunk.
+dim_chunk_sel
+    Selection of items from chunk array.
+dim_out_sel
+    Selection of items in target (output) array.
+"""
+
+
+def normalize_integer_selection(dim_sel, dim_len):
+
+    # normalize type to int
+    dim_sel = int(dim_sel)
+
+    # handle wraparound
+    if dim_sel < 0:
+        dim_sel = dim_len + dim_sel
+
+    # handle out of bounds
+    if dim_sel >= dim_len or dim_sel < 0:
+        err_boundscheck(dim_len)
+
+    return dim_sel
+
+
+class IntDimIndexer(object):
+
+    def __init__(self, dim_sel, dim_len, dim_chunk_len):
+
+        # normalize
+        dim_sel = normalize_integer_selection(dim_sel, dim_len)
+
+        # store attributes
+        self.dim_sel = dim_sel
+        self.dim_len = dim_len
+        self.dim_chunk_len = dim_chunk_len
+        self.nitems = 1
+
+    def __iter__(self):
+        dim_chunk_ix = self.dim_sel // self.dim_chunk_len
+        dim_offset = dim_chunk_ix * self.dim_chunk_len
+        dim_chunk_sel = self.dim_sel - dim_offset
+        dim_out_sel = None
+        yield ChunkDimProjection(dim_chunk_ix, dim_chunk_sel, dim_out_sel)
+
+
+def ceildiv(a, b):
+    return math.ceil(a / b)
+
+
+class SliceDimIndexer(object):
+
+    def __init__(self, dim_sel, dim_len, dim_chunk_len):
+
+        # normalize
+        self.start, self.stop, self.step = dim_sel.indices(dim_len)
+        if self.step < 1:
+            err_negative_step()
+
+        # store attributes
+        self.dim_len = dim_len
+        self.dim_chunk_len = dim_chunk_len
+        self.nitems = max(0, ceildiv((self.stop - self.start), self.step))
+        self.nchunks = ceildiv(self.dim_len, self.dim_chunk_len)
+
+    def __iter__(self):
+
+        # figure out the range of chunks we need to visit
+        dim_chunk_ix_from = self.start // self.dim_chunk_len
+        dim_chunk_ix_to = ceildiv(self.stop, self.dim_chunk_len)
+
+        # iterate over chunks in range
+        for dim_chunk_ix in range(dim_chunk_ix_from, dim_chunk_ix_to):
+
+            # compute offsets for chunk within overall array
+            dim_offset = dim_chunk_ix * self.dim_chunk_len
+            dim_limit = min(self.dim_len, (dim_chunk_ix + 1) * self.dim_chunk_len)
+
+            # determine chunk length, accounting for trailing chunk
+            dim_chunk_len = dim_limit - dim_offset
+
+            if self.start < dim_offset:
+                # selection starts before current chunk
+                dim_chunk_sel_start = 0
+                remainder = (dim_offset - self.start) % self.step
+                if remainder:
+                    dim_chunk_sel_start += self.step - remainder
+                # compute number of previous items, provides offset into output array
+                dim_out_offset = ceildiv((dim_offset - self.start), self.step)
+
+            else:
+                # selection starts within current chunk
+                dim_chunk_sel_start = self.start - dim_offset
+                dim_out_offset = 0
+
+            if self.stop > dim_limit:
+                # selection ends after current chunk
+                dim_chunk_sel_stop = dim_chunk_len
+
+            else:
+                # selection ends within current chunk
+                dim_chunk_sel_stop = self.stop - dim_offset
+
+            dim_chunk_sel = slice(dim_chunk_sel_start, dim_chunk_sel_stop, self.step)
+            dim_chunk_nitems = ceildiv((dim_chunk_sel_stop - dim_chunk_sel_start),
+                                       self.step)
+            dim_out_sel = slice(dim_out_offset, dim_out_offset + dim_chunk_nitems)
+
+            yield ChunkDimProjection(dim_chunk_ix, dim_chunk_sel, dim_out_sel)
+
+
+ChunkProjection = collections.namedtuple(
+    'ChunkProjection',
+    ('chunk_coords', 'chunk_selection', 'out_selection')
+)
+"""A mapping of items from chunk to output array. Can be used to extract items from the
+chunk array for loading into an output array. Can also be used to extract items from a
+value array for setting/updating in a chunk array.
+Parameters
+----------
+chunk_coords
+    Indices of chunk.
+chunk_selection
+    Selection of items from chunk array.
+out_selection
+    Selection of items in target (output) array.
+"""
+
+
+class BasicIndexer(object):
+
+    def __init__(self, selection, shape, chunk_shape):
+
+        # handle ellipsis
+        selection = replace_ellipsis(selection, shape)
+
+        # setup per-dimension indexers
+        dim_indexers = []
+        for dim_sel, dim_len, dim_chunk_len in zip(selection, shape, chunk_shape):
+
+            if isinstance(dim_sel, numbers.Integral):
+                dim_indexer = IntDimIndexer(dim_sel, dim_len, dim_chunk_len)
+
+            elif isinstance(dim_sel, slice):
+                dim_indexer = SliceDimIndexer(dim_sel, dim_len, dim_chunk_len)
+
+            else:
+                raise IndexError('unsupported selection item for basic indexing; '
+                                 'expected integer or slice, got {!r}'
+                                 .format(type(dim_sel)))
+
+            dim_indexers.append(dim_indexer)
+
+        self.dim_indexers = dim_indexers
+        self.shape = tuple(s.nitems for s in self.dim_indexers
+                           if not isinstance(s, IntDimIndexer))
+
+    def __iter__(self):
+        for dim_projections in itertools.product(*self.dim_indexers):
+
+            chunk_coords = tuple(p.dim_chunk_ix for p in dim_projections)
+            chunk_selection = tuple(p.dim_chunk_sel for p in dim_projections)
+            out_selection = tuple(p.dim_out_sel for p in dim_projections
+                                  if p.dim_out_sel is not None)
+
+            yield ChunkProjection(chunk_coords, chunk_selection, out_selection)
 
 
 class Store:
@@ -544,7 +1001,6 @@ class FileSystemStore(Store):
 
     def __setitem__(self, key, value):
         assert isinstance(key, str)
-        assert isinstance(value, bytes)
         path = f'{self.root}/{key}'
 
         # ensure parent folder exists
