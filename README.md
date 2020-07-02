8cfe110cacbaa359d139e31e59485942471a4638
Alistair Miles <alimanfoo@googlemail.com>
2020-07-02 16:12:39 +0100

    Iteration 1 (#1)
    * setup hierarchy test
    
    * file system store partially working
    
    * access group
    
    * work
    
    * list children
    
    * tweak doctests
    
    * fix rmtree
    
    * provide tree
    
    * tweaks

--- README.md
+++ README.md
@@ -1,15 +1,223 @@
-Here be dragons.
+**Here be dragons.**
 
-```python
->>> import zarr_v3
+Ensure blank slate:
 
+```
+>>> import shutil
+>>> shutil.rmtree('test.zr3', ignore_errors=True)
+ 
 ```
 
 Create a new hierarchy stored on the local file system:
 
-```python
+```
+>>> import zarr_v3
 >>> h = zarr_v3.create_hierarchy('test.zr3')
 >>> h
 <zarr_v3 Hierarchy>
+>>> from sh import tree, cat
+>>> tree('test.zr3', '-n')
+test.zr3
+└── zarr.json
+<BLANKLINE>
+0 directories, 1 file
+<BLANKLINE>
+
+>>> cat('test.zr3/zarr.json')
+{
+    "zarr_format": "https://purl.org/zarr/spec/protocol/core/3.0",
+    "metadata_encoding": "application/json",
+    "extensions": []
+}
+
+```
+
+Access a previously created hierarchy:
+
+```
+>>> h = zarr_v3.get_hierarchy('test.zr3')
+>>> h
+<zarr_v3 Hierarchy>
+
+```
+
+Create an array:
+
+```
+>>> a = h.create_array('/arthur/dent', shape=(100, 10), dtype='i4', chunk_shape=(20, 5), compressor=None, attrs={'question': 'life', 'answer': 42})
+>>> a
+<zarr_v3 Array /arthur/dent>
+>>> a.path
+'/arthur/dent'
+>>> a.ndim
+2
+>>> a.shape
+(100, 10)
+>>> a.dtype
+dtype('int32')
+>>> a.chunk_shape
+(20, 5)
+>>> a.compressor is None
+True
+>>> a.attrs
+{'question': 'life', 'answer': 42}
+>>> tree('test.zr3', '-n')
+test.zr3
+├── meta
+│   └── root
+│       └── arthur
+│           └── dent.array
+└── zarr.json
+<BLANKLINE>
+3 directories, 2 files
+<BLANKLINE>
+
+>>> cat('test.zr3/meta/root/arthur/dent.array')
+{
+    "shape": [
+        100,
+        10
+    ],
+    "data_type": "<i4",
+    "chunk_grid": {
+        "type": "regular",
+        "chunk_shape": [
+            20,
+            5
+        ]
+    },
+    "chunk_memory_layout": "C",
+    "compressor": null,
+    "fill_value": null,
+    "extensions": [],
+    "attributes": {
+        "question": "life",
+        "answer": 42
+    }
+}
+
+```
+
+Create a group:
+
+```
+>>> g = h.create_group('/tricia/mcmillan', attrs={'heart': 'gold', 'improbability': 'infinite'})
+>>> g
+<zarr_v3 Group /tricia/mcmillan>
+>>> g.attrs
+{'heart': 'gold', 'improbability': 'infinite'}
+>>> tree('test.zr3', '-n')
+test.zr3
+├── meta
+│   └── root
+│       ├── arthur
+│       │   └── dent.array
+│       └── tricia
+│           └── mcmillan.group
+└── zarr.json
+<BLANKLINE>
+4 directories, 3 files
+<BLANKLINE>
+
+>>> cat('test.zr3/meta/root/tricia/mcmillan.group')
+{
+    "extensions": [],
+    "attributes": {
+        "heart": "gold",
+        "improbability": "infinite"
+    }
+}
+
+```
+
+Access an array:
+
+```
+>>> a = h['/arthur/dent']
+>>> a
+<zarr_v3 Array /arthur/dent>
+>>> a.shape
+(100, 10)
+>>> a.dtype
+dtype('int32')
+>>> a.chunk_shape
+(20, 5)
+>>> a.compressor is None
+True
+>>> a.attrs
+{'question': 'life', 'answer': 42}
+
+```
+
+Access an explicit group:
+
+```
+>>> g = h['/tricia/mcmillan']
+>>> g
+<zarr_v3 Group /tricia/mcmillan>
+>>> g.attrs
+{'heart': 'gold', 'improbability': 'infinite'}
+
+```
+
+Access implicit groups:
+
+```
+>>> h['/']
+<zarr_v3 Group / (implied)>
+>>> h['/arthur']
+<zarr_v3 Group /arthur (implied)>
+>>> h['/tricia']
+<zarr_v3 Group /tricia (implied)>
+
+```
+
+Access nodes via groups:
+
+```
+>>> root = h['/']
+>>> root
+<zarr_v3 Group / (implied)>
+>>> arthur = root['arthur']
+>>> arthur
+<zarr_v3 Group /arthur (implied)>
+>>> arthur['dent']
+<zarr_v3 Array /arthur/dent>
+>>> tricia = root['tricia']
+>>> tricia
+<zarr_v3 Group /tricia (implied)>
+>>> tricia['mcmillan']
+<zarr_v3 Group /tricia/mcmillan>
+
+```
+
+Explore the hierarchy:
+
+```
+>>> h.list_children('/')
+[{'name': 'arthur', 'type': 'implicit_group'}, {'name': 'tricia', 'type': 'implicit_group'}]
+>>> h.list_children('/tricia')
+[{'name': 'mcmillan', 'type': 'explicit_group'}]
+>>> h.list_children('/tricia/mcmillan')
+[]
+>>> h.list_children('/arthur')
+[{'name': 'dent', 'type': 'array'}]
+
+```
+
+Alternative way to explore the hierarchy:
+
+```
+>>> root = h['/']
+>>> root
+<zarr_v3 Group / (implied)>
+>>> root.list_children()
+[{'name': 'arthur', 'type': 'implicit_group'}, {'name': 'tricia', 'type': 'implicit_group'}]
+>>> root['tricia'].list_children()
+[{'name': 'mcmillan', 'type': 'explicit_group'}]
+>>> root['tricia']['mcmillan'].list_children()
+[]
+>>> root['arthur'].list_children()
+[{'name': 'dent', 'type': 'array'}]
 
 ```
--- zarr_v3.py
+++ zarr_v3.py
@@ -65,7 +65,7 @@ def get_hierarchy(store):
     # check protocol version
     zarr_format = meta['zarr_format']
     protocol_version = zarr_format.split('/')[-1]
-    protocol_major_version = protocol_version.split('.')[0]
+    protocol_major_version = int(protocol_version.split('.')[0])
     if protocol_major_version != 3:
         raise NotImplementedError
 
@@ -89,7 +89,8 @@ def get_hierarchy(store):
 def _check_path(path):
     assert isinstance(path, str)
     assert path[0] == '/'
-    assert path[-1] != '/'
+    if len(path) > 1:
+        assert path[-1] != '/'
 
 
 def _check_attrs(attrs):
@@ -116,17 +117,20 @@ def _check_dtype(dtype):
     return dtype
 
 
-def _check_chunks(chunks, shape):
-    assert isinstance(chunks, tuple)
-    assert all([isinstance(c, int) for c in chunks])
-    assert len(chunks) == len(shape)
+def _check_chunk_shape(chunk_shape, shape):
+    assert isinstance(chunk_shape, tuple)
+    assert all([isinstance(c, int) for c in chunk_shape])
+    assert len(chunk_shape) == len(shape)
 
 
 def _check_compressor(compressor):
-    assert isinstance(compressor, numcodecs.abc.Codec)
+    assert compressor is None or isinstance(compressor, numcodecs.abc.Codec)
 
 
-def _get_codec_metadata(codec):
+def _encode_codec(codec):
+    if codec is None:
+        return None
+
     # only support gzip for now
     assert codec.codec_id in {'gzip'}
     config = codec.get_config()
@@ -136,6 +140,17 @@ def _get_codec_metadata(codec):
         'configuration': config,
     }
     return meta
+
+
+def _decode_codec(meta):
+    if meta is None:
+        return None
+
+    # only support gzip for now
+    if meta['codec'] != 'https://purl.org/zarr/spec/codec/gzip/1.0':
+        raise NotImplementedError
+    codec = numcodecs.GZip(level=meta['level'])
+    return codec
 
 
 class Hierarchy:
@@ -159,10 +174,9 @@ class Hierarchy:
         meta_doc = _json_encode(meta)
         if path == '/':
             # special case root path
-            meta_key_suffix = 'root.group'
+            meta_key = 'meta/root.group'
         else:
-            meta_key_suffix = 'root' + path + '.group'
-        meta_key = 'meta/' + meta_key_suffix
+            meta_key = f'meta/root{path}.group'
         self.store[meta_key] = meta_doc
 
         # instantiate group
@@ -171,13 +185,14 @@ class Hierarchy:
 
         return group
 
-    def create_array(self, path, shape, dtype, chunks, compressor, attrs=None):
+    def create_array(self, path, shape, dtype, chunk_shape, compressor,
+                     fill_value=None, attrs=None):
 
         # sanity checks
         _check_path(path)
         _check_shape(shape)
         dtype = _check_dtype(dtype)
-        _check_chunks(chunks, shape)
+        _check_chunk_shape(chunk_shape, shape)
         _check_compressor(compressor)
         _check_attrs(attrs)
 
@@ -193,11 +208,11 @@ class Hierarchy:
             'data_type': data_type,
             'chunk_grid': {
                 'type': 'regular',
-                'chunk_shape': chunks,
+                'chunk_shape': chunk_shape,
             },
             'chunk_memory_layout': 'C',
-            'compressor': _get_codec_metadata(compressor),
-            'fill_value': None,
+            'compressor': _encode_codec(compressor),
+            'fill_value': fill_value,
             'extensions': [],
             'attributes': attrs,
         }
@@ -206,49 +221,95 @@ class Hierarchy:
         meta_doc = _json_encode(meta)
         if path == '/':
             # special case root path
-            meta_key_suffix = 'root.array'
+            meta_key = 'meta/root.array'
         else:
-            meta_key_suffix = 'root' + path + '.array'
-        meta_key = 'meta/' + meta_key_suffix
+            meta_key = f'meta/root{path}.array'
         self.store[meta_key] = meta_doc
 
         # instantiate array
         array = Array(store=self.store, path=path, owner=self,
-                      shape=shape, dtype=dtype, chunks=chunks,
-                      compressor=compressor, attrs=attrs)
+                      shape=shape, dtype=dtype, chunk_shape=chunk_shape,
+                      compressor=compressor, fill_value=fill_value,
+                      attrs=attrs)
 
         return array
 
     def get_array(self, path):
         _check_path(path)
 
-        # TODO retrieve and parse array metadata document
+        # retrieve and parse array metadata document
+        if path == '/':
+            meta_key = 'meta/root.array'
+        else:
+            meta_key = f'meta/root{path}.array'
+        try:
+            meta_doc = self.store[meta_key]
+        except KeyError:
+            raise NodeNotFoundError(path)
+        meta = _json_decode(meta_doc)
 
-        # TODO decode and check metadata
+        # decode and check metadata
+        shape = tuple(meta['shape'])
+        dtype = np.dtype(meta['data_type'])
+        chunk_grid = meta['chunk_grid']
+        if chunk_grid['type'] != 'regular':
+            raise NotImplementedError
+        chunk_shape = tuple(chunk_grid['chunk_shape'])
+        chunk_memory_layout = meta['chunk_memory_layout']
+        if chunk_memory_layout != 'C':
+            raise NotImplementedError
+        compressor = _decode_codec(meta['compressor'])
+        fill_value = meta['fill_value']
+        for spec in meta['extensions']:
+            if spec['must_understand']:
+                raise NotImplementedError
+        attrs = meta['attributes']
 
-        # TODO instantiate array
+        # instantiate array
+        a = Array(store=self.store, path=path, owner=self, shape=shape,
+                  dtype=dtype, chunk_shape=chunk_shape, compressor=compressor,
+                  fill_value=fill_value, attrs=attrs)
 
-        pass
+        return a
 
     def get_explicit_group(self, path):
         _check_path(path)
 
-        # TODO retrieve and parse group metadata document
+        # retrieve and parse group metadata document
+        if path == '/':
+            meta_key = 'meta/root.group'
+        else:
+            meta_key = f'meta/root{path}.group'
+        try:
+            meta_doc = self.store[meta_key]
+        except KeyError:
+            raise NodeNotFoundError(path)
+        meta = _json_decode(meta_doc)
 
-        # TODO check metadata
+        # check metadata
+        attrs = meta['attributes']
 
-        # TODO instantiate explicit group
+        # instantiate explicit group
+        g = ExplicitGroup(store=self.store, path=path, owner=self, attrs=attrs)
 
-        pass
+        return g
 
     def get_implicit_group(self, path):
         _check_path(path)
 
-        # TODO attempt to list directory
+        # attempt to list directory
+        if path == '/':
+            key_prefix = 'meta/root/'
+        else:
+            key_prefix = f'meta/root{path}/'
+        result = self.store.list_dir(key_prefix)
+        if not (result['contents'] or result['prefixes']):
+            raise NodeNotFoundError(path)
 
-        # TODO instantiate implicit group
+        # instantiate implicit group
+        g = ImplicitGroup(store=self.store, path=path, owner=self)
 
-        pass
+        return g
 
     def __getitem__(self, path):
 
@@ -274,10 +335,47 @@ class Hierarchy:
 
     def __repr__(self):
         return '<zarr_v3 Hierarchy>'
+    
+    def list_children(self, path):
+        _check_path(path)
+        
+        # attempt to list directory
+        if path == '/':
+            key_prefix = 'meta/root/'
+        else:
+            key_prefix = f'meta/root{path}/'
+        result = self.store.list_dir(key_prefix)
+        
+        # compile children
+        children = []
+        names = set()
+
+        # find explicit children
+        for n in sorted(result['contents']):
+            if n.endswith('.array'):
+                node_type = 'array'
+                name = n[:-len('.array')]
+            elif n.endswith('.group'):
+                node_type = 'explicit_group'
+                name = n[:-len('.group')]
+            else:
+                # ignore
+                continue
+            children.append({'name': name, 'type': node_type})
+            names.add(name)
+
+        # find implicit children
+        for n in sorted(result['prefixes']):
+            if n not in names:
+                children.append({'name': n, 'type': 'implicit_group'})
+
+        return children
 
 
 class NodeNotFoundError(Exception):
-    pass
+
+    def __init__(self, path):
+        self.path = path
 
 
 class Group:
@@ -287,22 +385,32 @@ class Group:
         self.path = path
         self.owner = owner
 
-    def list_children(self, path):
-        pass
+    def list_children(self):
+        return self.owner.list_children(self.path)
+
+    def _dereference_path(self, path):
+        assert isinstance(path, str)
+        if path[0] != '/':
+            # treat as relative path
+            if self.path == '/':
+                # special case root group
+                path = f'/{path}'
+            else:
+                path = f'{self.path}/{path}'
+        if len(path) > 1:
+            assert path[-1] != '/'
+        return path
 
     def __getitem__(self, path):
-        # pass through to owner
-        _check_path(path)
-        return self.owner[self.path + path]
+        path = self._dereference_path(path)
+        return self.owner[path]
 
     def create_group(self, path, **kwargs):
-        # pass through to owner
-        _check_path(path)
+        path = self._dereference_path(path)
         return self.owner.create_group(self.path + path, **kwargs)
 
     def create_array(self, path, **kwargs):
-        # pass through to owner
-        _check_path(path)
+        path = self._dereference_path(path)
         return self.owner.create_array(self.path + path, **kwargs)
     
     def get_array(self, path):
@@ -341,23 +449,28 @@ class ImplicitGroup(Group):
 
     def __repr__(self):
         path = self.path
-        return f'<zarr_v3 Group (implied) {path}>'
+        return f'<zarr_v3 Group {path} (implied)>'
 
 
 class Array:
 
     # TODO persist changes to attrs
 
-    def __init__(self, store, path, owner, shape, dtype, chunks,
-                 compressor, attrs):
+    def __init__(self, store, path, owner, shape, dtype, chunk_shape,
+                 compressor, fill_value, attrs):
         self.store = store
         self.path = path
         self.owner = owner
         self.shape = shape
         self.dtype = dtype
-        self.chunks = chunks
+        self.chunk_shape = chunk_shape
         self.compressor = compressor
+        self.fill_value = fill_value
         self.attrs = attrs
+
+    @property
+    def ndim(self):
+        return len(self.shape)
 
     def __getitem__(self, item):
         # TODO
@@ -404,24 +517,43 @@ class Store:
 
 class FileSystemStore(Store):
 
-    def __init__(self, fs, prefix=''):
-        if isinstance(fs, str):
-            # TODO instantiate file system
-            pass
-        assert isinstance(prefix, str)
-        self.fs = fs
-        self.prefix = prefix
+    # TODO ultimately replace this with the fsspec FSMap class, but for now roll
+    # our own implementation in order to be able to add some extra methods for
+    # listing keys.
 
-    def __getitem__(self, key):
+    def __init__(self, url, **kwargs):
+        assert isinstance(url, str)
+
+        # instantiate file system
+        fs, root = fsspec.core.url_to_fs(url, **kwargs)
+        self.fs = fs
+        self.root = root.rstrip('/')
+
+    def __getitem__(self, key, default=None):
         assert isinstance(key, str)
-        # TODO
-        pass
+        path = f'{self.root}/{key}'
+
+        try:
+            value = self.fs.cat(path)
+        except (FileNotFoundError, IsADirectoryError, NotADirectoryError):
+            if default is not None:
+                return default
+            raise KeyError(key)
+
+        return value
 
     def __setitem__(self, key, value):
         assert isinstance(key, str)
         assert isinstance(value, bytes)
-        # TODO
-        pass
+        path = f'{self.root}/{key}'
+
+        # ensure parent folder exists
+        # noinspection PyProtectedMember
+        self.fs.mkdirs(self.fs._parent(path), exist_ok=True)
+
+        # write data
+        with self.fs.open(path, 'wb') as f:
+            f.write(value)
 
     def __delitem__(self, key):
         assert isinstance(key, str)
@@ -448,7 +580,28 @@ class FileSystemStore(Store):
         # TODO
         pass
 
-    def list_dir(self, prefix):
+    def list_dir(self, prefix=''):
         assert isinstance(prefix, str)
-        # TODO
-        pass
+
+        # setup result
+        result = {
+            'contents': [],
+            'prefixes': [],
+        }
+
+        # attempt to list directory
+        path = f'{self.root}/{prefix}'
+        try:
+            ls = self.fs.ls(path, detail=True)
+        except FileNotFoundError:
+            return result
+
+        # build result
+        for item in ls:
+            name = item['name'].split(path)[1]
+            if item['type'] == 'file':
+                result['contents'].append(name)
+            elif item['type'] == 'directory':
+                result['prefixes'].append(name)
+
+        return result
