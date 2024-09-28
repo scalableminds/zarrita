884150ee053b600340f6c0def2034bc543860d43
brokkoli71 <brokkoli71b@gmail.com>
2024-09-28 14:18:09 +0200

    set default typesize=None

--- zarrita/metadata.py
+++ zarrita/metadata.py
@@ -149,7 +149,7 @@ BloscShuffle = Literal["noshuffle", "shuffle", "bitshuffle"]
 
 @frozen
 class BloscCodecConfigurationMetadata:
-    typesize: int = 0
+    typesize: int | None = None
     cname: Literal["lz4", "lz4hc", "blosclz", "zstd", "snappy", "zlib"] = "zstd"
     clevel: int = 5
     shuffle: BloscShuffle = "noshuffle"
